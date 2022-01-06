96906af80a65548bfc17d26febea732bb755f60c
khouari1 <karim.houari@genesis.global>
2022-01-06 11:29:35 +0000

    Update download urls to use github releases

--- dist/index.js
+++ dist/index.js
@@ -480,21 +480,21 @@ function exec(cmd) {
 try {
     const version = core.getInput('version');
     console.log(`Installing foundationdb ${version} (${os.platform()})!`);
-    let base_url = `https://www.foundationdb.org/downloads/${version}`;
+    let base_url = `https://github.com/apple/foundationdb/releases/download/${version}`;
     switch (os.platform()) {
         case 'linux': {
-            let client_url = `${base_url}/ubuntu/installers/foundationdb-clients_${version}-1_amd64.deb`;
-            exec(`curl -O ${client_url}`);
+            let client_url = `${base_url}/foundationdb-clients_${version}-1_amd64.deb`;
+            exec(`curl -L -O ${client_url}`);
             exec(`sudo dpkg -i foundationdb-clients_${version}-1_amd64.deb`);
 
-            let server_url = `${base_url}/ubuntu/installers/foundationdb-server_${version}-1_amd64.deb`;
-            exec(`curl -O ${server_url}`);
+            let server_url = `${base_url}/foundationdb-server_${version}-1_amd64.deb`;
+            exec(`curl -L -O ${server_url}`);
             exec(`sudo dpkg -i foundationdb-server_${version}-1_amd64.deb`);
             break;
         }
         case 'darwin': {
-            let url = `${base_url}/macOS/installers/FoundationDB-${version}.pkg`;
-            exec(`curl -O ${url}`);
+            let url = `${base_url}/FoundationDB-${version}.pkg`;
+            exec(`curl -L -O ${url}`);
             exec(`sudo installer -pkg FoundationDB-${version}.pkg -target /`);
             break;
         }
